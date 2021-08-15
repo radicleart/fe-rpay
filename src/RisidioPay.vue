f25771da8f7aceafada537a3a8b186fc2a840a36
Mike Cohen <mjoecohen@gmail.com>
2021-08-15 18:22:33 +0100

    The big chop - removed local vue component code and dependencies

--- src/RisidioPay.vue
+++ src/RisidioPay.vue
@@ -1,30 +1,10 @@
 <template>
-<div class="" v-if="loaded" id="rpay-pay-card">
-  <div class="col-12" v-if="risidioCardMode === 'payment-flow'">
-    <payment-flow />
-  </div>
-  <div class="col-12" v-else-if="risidioCardMode === 'minting-flow'">
-    <minting-flow/>
-  </div>
-  <div class="col-12" v-else-if="risidioCardMode === 'selling-flow'">
-    <selling-flow v-if="configured" :gaiaAsset="gaiaAsset"/>
-  </div>
-  <div class="col-12" v-else-if="risidioCardMode === 'purchase-flow'">
-    <purchase-flow v-if="configured" :gaiaAsset="gaiaAsset"/>
-  </div>
-  <div class="col-12 text-white" v-if="showDebug">
-    <debug-flow/>
-  </div>
+<div>
 </div>
 </template>
 
 <script>
 import Vue from 'vue'
-import DebugFlow from './views/debug-screens/DebugFlow'
-import PaymentFlow from './views/PaymentFlow'
-import MintingFlow from './views/MintingFlow'
-import SellingFlow from './views/SellingFlow'
-import PurchaseFlow from './views/PurchaseFlow'
 import rpayStore from './store/rpayStore'
 import rpaySearchStore from '@/store/rpaySearchStore'
 import rpayEthereumStore from './store/rpayEthereumStore'
@@ -36,7 +16,6 @@ import rpayPrivilegeStore from './store/rpayPrivilegeStore'
 import rpayStacksContractStore from './store/rpayStacksContractStore'
 import rpayStacksStore from './store/rpayStacksStore'
 import rpayPurchaseStore from './store/rpayPurchaseStore'
-import { APP_CONSTANTS } from '@/app-constants'
 
 if (!window.eventBus) {
   window.eventBus = new Vue()
@@ -44,19 +23,12 @@ if (!window.eventBus) {
 export default {
   name: 'RisidioPay',
   components: {
-    PaymentFlow,
-    MintingFlow,
-    SellingFlow,
-    PurchaseFlow,
-    DebugFlow
   },
   props: ['configuration'],
   data () {
     return {
       configured: false,
-      loaded: false,
-      showDebug: false,
-      risidioCardMode: 'mode-payments'
+      loaded: false
     }
   },
   beforeDestroy () {
@@ -69,9 +41,6 @@ export default {
       configuration = this.parseConfiguration()
     }
     // only register the vuex store modules if not already registered..
-    const urlParams = new URLSearchParams(window.location.search)
-    const showDebug = urlParams.get('debug')
-    this.showDebug = showDebug === 'true'
     const agetter = this.$store.getters['rpayStore/getDisplayCard']
     if (!agetter) {
       this.$store.registerModule('rpaySearchStore', rpaySearchStore)
@@ -90,13 +59,6 @@ export default {
     this.$store.dispatch('rpayStacksContractStore/fetchContractDataFirstEditions', configuration).then(() => {
       this.configured = true
     })
-    // parse and store the main configuration object
-    const lf = (configuration.lookAndFeel) ? configuration.lookAndFeel : this.defLF()
-    if (!lf.variant) lf.variant = 'warning'
-    if (!lf.variant1) lf.variant1 = 'danger'
-    if (!lf.variant2) lf.variant2 = 'info'
-    Vue.prototype.$globalLookAndFeel = lf
-    this.risidioCardMode = configuration.risidioCardMode
     this.$store.commit('rpayStore/addConfiguration', configuration)
     this.$store.commit('rpayStore/setDisplayCard', 100) // initial screen for each flow.
     this.$store.dispatch('rpayStore/initialiseRpayModule', configuration)
@@ -123,49 +85,11 @@ export default {
         }
       }
       return paymentConfig
-    },
-    defLF: function () {
-      return {
-        labels: {
-          variant0: 'danger',
-          variant1: 'warning',
-          variant2: 'info',
-          variant3: 'light',
-          labels: {
-            title: 'Mint Your Item',
-            numberUnits: 'How many spins?',
-            quantityLabel: 'Tokens'
-          },
-          numberUnits: 'How many items?',
-          orderMsg: 'Place order for \'STX to Lightning\' select number required and pay.',
-          successMsg: 'Your order has been received with thanks.',
-          title: 'dsfsfsdf',
-          subtitle: 'Republic of Movies',
-          card1Label: 'Select amount to send',
-          card2Label2: 'Select operation',
-          card2Label3: 'Make Payment',
-          card2Label4: 'Open Channel',
-          button1Label: 'Back',
-          button2Label: 'Next',
-          quantityLabel: 'Tokens'
-        }
-      }
     }
   },
   computed: {
-    gaiaAsset () {
-      const configuration = this.$store.getters[APP_CONSTANTS.KEY_CONFIGURATION]
-      return configuration.gaiaAsset
-    },
-    loginRequired () {
-      const configuration = this.$store.getters[APP_CONSTANTS.KEY_CONFIGURATION]
-      const profile = this.$store.getters[APP_CONSTANTS.KEY_PROFILE]
-      return configuration.loginRequired > 0 && !profile.loggedIn
-    }
   }
 }
 </script>
 <style lang="scss" scoped>
-@import '~vue-datetime/dist/vue-datetime.css'
-
 </style>
